294ea44fe6a8be33e34129c97e87b58c48d666cf
cinexin <migueguillamon@gmail.com>
2017-03-11 11:48:15 +0100

    Añadido Action Bar para la pantalla principal. Redimensionado de iconos (un poco mas grandes)

--- app/src/main/java/upv/ejercicios/proyectofinal/quierosermillonario/MainScreen.java
+++ app/src/main/java/upv/ejercicios/proyectofinal/quierosermillonario/MainScreen.java
@@ -1,13 +1,21 @@
 package upv.ejercicios.proyectofinal.quierosermillonario;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainScreen extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainScreen extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_screen, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
