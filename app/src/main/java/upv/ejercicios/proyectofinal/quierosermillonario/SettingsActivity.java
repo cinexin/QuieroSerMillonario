b62cb78aee9d206c1557f419c0e1f4ce33d428d4
cinexin <migueguillamon@gmail.com>
2017-03-17 10:03:41 +0100

    Settings functionality

--- app/src/main/java/upv/ejercicios/proyectofinal/quierosermillonario/SettingsActivity.java
+++ app/src/main/java/upv/ejercicios/proyectofinal/quierosermillonario/SettingsActivity.java
@@ -33,7 +33,7 @@ public class SettingsActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 // TODO: Collect input data and Call GameSettingsService.saveSettings
-                
+
             }
         });
     }
