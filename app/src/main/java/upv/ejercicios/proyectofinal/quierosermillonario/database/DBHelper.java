db1358191306c9563895eec23f843f3f8ebc9f28
cinexin <migueguillamon@gmail.com>
2017-03-21 18:13:57 +0100

    Database management implementation to manage scores (basically). First commit

---
+++ app/src/main/java/upv/ejercicios/proyectofinal/quierosermillonario/database/DBHelper.java
@@ -0,0 +1,21 @@
+package upv.ejercicios.proyectofinal.quierosermillonario.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by migui on 0021.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/upv/ejercicios/proyectofinal/quierosermillonario/model/GameScore.java
+++ app/src/main/java/upv/ejercicios/proyectofinal/quierosermillonario/model/GameScore.java
@@ -6,9 +6,12 @@ package upv.ejercicios.proyectofinal.quierosermillonario.model;
 
 public class GameScore {
     int lastQuestionAnswered;
-    int moneyAchieved;
-    int playingFor;
+    int moneyAchieved; // that is, the "score"
+    int playingFor; // the value of the current question
     int moneyEnsured; // 5th and 10th questions ensures you a quantity (500 € / 16.000 €)
+    String userName; // user name in case of "non-anonymous" gamer
+    float longitude; // user's location (longitude)
+    float latitude; // user's location (latitude)
 
     public GameScore() {
 
@@ -19,6 +22,40 @@ public class GameScore {
         this.moneyAchieved = moneyAchieved;
         this.playingFor = playingFor;
         this.moneyEnsured = moneyEnsured;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public GameScore(int lastQuestionAnswered, int moneyAchieved, int playingFor, int moneyEnsured, String userName, float longitude, float latitude) {
+        this.lastQuestionAnswered = lastQuestionAnswered;
+        this.moneyAchieved = moneyAchieved;
+        this.playingFor = playingFor;
+        this.moneyEnsured = moneyEnsured;
+        this.userName = userName;
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 
     public int getLastQuestionAnswered() {
@@ -60,6 +97,9 @@ public class GameScore {
                 ", moneyAchieved=" + moneyAchieved +
                 ", playingFor=" + playingFor +
                 ", moneyEnsured=" + moneyEnsured +
-                '}';
+                ", userName = " + userName +
+                ", longitude = " + longitude +
+                ", latitude = " + latitude +
+        '}';
     }
 }
